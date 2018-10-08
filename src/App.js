40e504d36b11bdc936eeffcffe393718762319d2
sahilmazmudar <sahil.maz97@gmail.com>
2018-10-08 14:11:18 -0700

    adds barbone implementation for average of just two numbers

--- src/App.js
+++ src/App.js
@@ -2,18 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getAverage(props){
+  <h1> Average is: {parseInt((props.course1)+(props.course2))/2}</h1>
+}
+
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props);
+    this.state = {
+      firstCourse: 0,
+      secondCourse: 0,
+      average: 0
+    };
+    this.handleCourse1Change = this.handleCourse1Change.bind(this);
+    this.handleCourse2Change = this.handleCourse2Change.bind(this); 
+  }
+  
+  handleCourse1Change (event) {
+    this.setState({firstCourse: Number(event.target.value)});
+  }
+
+  handleCourse2Change (event) {
+    this.setState({secondCourse: Number(event.target.value)});
+  }
+
+  addAction = (event) => {
+    let x = (this.state.firstCourse + this.state.secondCourse)/2
+    this.setState({average: x})
+  }
+  
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form>
+        <label>Enter course1:
+          <input value={this.state.firstCourse} 
+             name="firstCourse"
+             type="number"
+             onChange={this.handleCourse1Change}/>
+        </label>
+        <label>Enter course2:
+          <input value={this.state.secondCourse} 
+             name="secondCourse"
+             type="number"
+             onChange={this.handleCourse2Change}/>
+        </label> 
+        <input type="button" onClick={this.addAction} value="Average"/>
+        <input type='text' value={this.state.average} readOnly/>        
+      </form>
     );
   }
 }
