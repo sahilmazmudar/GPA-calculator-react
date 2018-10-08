222993850a1d4f16129b2a7d8808aa6489c5c129
sahilmazmudar <sahil.maz97@gmail.com>
2018-10-08 15:14:10 -0700

    adds weighted calculation for credits. Need to DRY

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function getAverage(props){
-  <h1> Average is: {parseInt((props.course1)+(props.course2))/2}</h1>
-}
-
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
       course1: '',
+      credits1: '',
       course2: '',
+      credits2: '',
       course3: '',
+      credits3: '',
       course4: '',
+      credits4: '',
       course5: '',
+      credits5: '',
       average: 0
     };
     this.handleChange = this.handleChange.bind(this);
@@ -27,10 +28,19 @@ class App extends Component {
   }
 
   averageAction = (event) => {
-    let x = (this.state.course1 + this.state.course2 + this.state.course3 + this.state.course4 + this.state.course5)/5
-    this.setState({average: x})
+    let rawTotal = (this.state.course1*this.state.credits1) + 
+                      (this.state.course2*this.state.credits2) +
+                      (this.state.course3*this.state.credits3) + 
+                      (this.state.course4*this.state.credits4) +
+                      (this.state.course5*this.state.credits5)
+    let totalCredits = this.state.credits1+this.state.credits2+this.state.credits3+this.state.credits4+this.state.credits5
+    let wightedAverage = rawTotal/totalCredits
+    this.setState({average: wightedAverage})
   }
   
+  weightedAverage = (event) => {
+
+  }
   render () {  
     return (
       <div>
@@ -40,10 +50,22 @@ class App extends Component {
                name="course1"
                type="text"
                onChange={this.handleChange}/>
+          </label>
+          <label>Enter credits for course1:
+            <input value={this.state.credits1} 
+               name="credits1"
+               type="text"
+               onChange={this.handleChange}/>
           </label><br/>
           <label>Enter course2:
             <input value={this.state.course2} 
                name="course2"
+               type="text"
+               onChange={this.handleChange}/>
+          </label>
+          <label>Enter credits for course2:
+            <input value={this.state.credits2} 
+               name="credits2"
                type="text"
                onChange={this.handleChange}/>
           </label><br/>
@@ -52,10 +74,22 @@ class App extends Component {
                name="course3"
                type="text"
                onChange={this.handleChange}/>
+          </label>
+          <label>Enter credits for course3:
+            <input value={this.state.credits3} 
+               name="credits3"
+               type="text"
+               onChange={this.handleChange}/>
           </label><br/>
           <label>Enter course4:
             <input value={this.state.course4} 
                name="course4"
+               type="text"
+               onChange={this.handleChange}/>
+          </label>
+          <label>Enter credits for course4:
+            <input value={this.state.credits4} 
+               name="credits4"
                type="text"
                onChange={this.handleChange}/>
           </label><br/>
@@ -64,7 +98,13 @@ class App extends Component {
                name="course5"
                type="text"
                onChange={this.handleChange}/>
-          </label>          
+          </label>
+          <label>Enter credits for course5:
+            <input value={this.state.credits5} 
+               name="credits5"
+               type="text"
+               onChange={this.handleChange}/>
+          </label><br/>      
         </form>
         <input type="button" onClick={this.averageAction} value="Average"/>
         <input type='text' value={this.state.average} readOnly/> 
